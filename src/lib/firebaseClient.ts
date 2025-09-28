7ebe744bd9eb5dbfc59a026e650121da4789eaf8
Silvio Luiz Fernandes Junior <silvio.slfj@gmail.com>
2025-09-28 15:50:24 +0000

    estou usando o Hosting e não o app Hosting no ultimo passo 3.

---
+++ src/lib/firebaseClient.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDUjqbK3i6d78mG2waoAeBmb8Ns8r_0Cws",
+  authDomain: "studio-4746052214-a7a40.firebaseapp.com",
+  projectId: "studio-4746052214-a7a40",
+  storageBucket: "studio-4746052214-a7a40.firebasestorage.app",
+  messagingSenderId: "126928125027",
+  appId: "1:126928125027:web:8275f2c7a07fdabe840c56"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
