2f1ce282019c0dd0b6735a3bbccf170a3c6b0287
Silvio Luiz Fernandes Junior <silvio.slfj@gmail.com>
2025-09-28 03:57:48 +0000

    Try fixing this error: `Runtime Error: Error: Input is not defined. Erro

--- src/app/payroll/page.tsx
+++ src/app/payroll/page.tsx
@@ -4,6 +4,7 @@
 import { useState } from "react";
 import { AppLayout } from "@/components/layout/app-layout";
 import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
 import {
   Card,
   CardContent,
